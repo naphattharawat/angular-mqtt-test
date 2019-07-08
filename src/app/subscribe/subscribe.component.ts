5f74e7b710c84b033c7e00cb9bab9bcb99eb7a7f
naphattharawat <naphattharawat@gmail.com>
2019-07-08 10:28:14 +0700

    up

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MqttClient } from 'mqtt';
+import * as mqttClient from '../../../mqtt/mqtt.min.js';
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
@@ -8,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class SubscribeComponent implements OnInit {
 
   no = 1;
+  mqttClient: MqttClient;
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.connectMqtt();
+    await this.subscribeMqtt();
+    await this.messageMqtt();
   }
 
+  connectMqtt() {
+    try {
+      this.mqttClient = new mqttClient('mqtt://localhost:8883', {
+        clienId: Math.floor(Math.random() * 10000),
+        username: 'mqtt',
+        password: 'password'
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  subscribeMqtt() {
+    const topic = 'test';
+    const that = this;
+    this.mqttClient.on('connect', () => {
+      that.mqttClient.subscribe(topic, (err) => {
+        if (err) {
+          console.log('Subscribe Error!!');
+        }
+      });
+    });
+  }
+
+  messageMqtt() {
+    this.mqttClient.on('message', (topic, payload) => {
+      this.no++;
+    });
+  }
 }
